f3f1deb94cdddae327dfc1250ffdf80e1fb634d8
தமிழ்நேரம் <67970539+TamilNeram@users.noreply.github.com>
2026-08-31 23:11:35 +0530

    Pip Updated

---
+++ App/PyScripter/remserver.py
@@ -0,0 +1,95 @@
+import sys
+if len(sys.argv) > 2:
+    sys.path.insert(0, sys.argv[2])
+
+from rpyc.utils.server import Server
+from rpyc.utils.classic import DEFAULT_SERVER_PORT
+from rpyc.core import SlaveService
+from rpyc.core.stream import NamedPipeStream
+from rpyc.utils.factory import connect_stream
+import threading
+
+__traceable__ = 0
+
+class SimpleServer(Server):
+    def _accept_method(self, sock):
+        try:
+            self._serve_client(sock, None)
+        finally:
+            self.close()
+
+class ModSlaveService(SlaveService):
+    __slots__ = []
+
+    def on_connect(self, conn):
+        import types
+        from rpyc.core.service import ModuleNamespace
+
+        sys.modules["__oldmain__"] = sys.modules["__main__"]
+        sys.modules["__main__"] = types.ModuleType("__main__")
+        self.namespace = sys.modules["__main__"].__dict__
+
+        conn._config.update(dict(
+            allow_all_attrs = True,
+            allow_pickle = True,
+            allow_getattr = True,
+            allow_setattr = True,
+            allow_delattr = True,
+            allow_exposed_attrs = False,
+            import_custom_exceptions = True,
+            instantiate_custom_exceptions = True,
+            instantiate_oldstyle_exceptions = True,
+            sync_request_timeout = None,
+        ))
+
+        # disable compression
+        conn._channel.compress = False
+        self._conn = conn
+
+        # shortcuts
+        conn.modules = ModuleNamespace(conn.root.getmodule)
+        conn.eval = conn.root.eval
+        conn.execute = conn.root.execute
+        conn.namespace = conn.root.namespace
+        conn.builtin = conn.modules.builtins
+
+##def printcrt(s):
+##    import msvcrt
+##    for c in s:
+##        msvcrt.putwch(c)
+
+def main():
+    import warnings
+    warnings.simplefilter("ignore", DeprecationWarning)
+
+    try:
+        port = int(sys.argv[1])
+    except:
+        port = DEFAULT_SERVER_PORT
+
+    conn = None
+    if port > 19000:
+        # Named server
+        try:
+            __import__("win32file")
+            np_server = NamedPipeStream.create_server(str(port))
+            conn = connect_stream(np_server, ModSlaveService)
+            try:
+                conn.serve_all()
+            except Exception:
+                pass
+            finally:
+                if (conn is not None) and not conn.closed:
+                    conn.close()
+                exit()
+        except SystemExit:
+            raise
+        except:
+            conn = None
+
+    if conn is None:
+        t = SimpleServer(ModSlaveService, port = port, auto_register = False)
+        t.start()
+
+if __name__ == "__main__":
+    main()
